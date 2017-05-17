509edef4670ca44d87f92ab12e52cbac9492a264
Joseph P DeBartola III <josephdebartola@gmail.com>
2017-05-17 07:50:32 -0500

    Change hyphens to equals signs in Request yield

--- wesearchr/spiders/wesearchr.py
+++ wesearchr/spiders/wesearchr.py
@@ -6,7 +6,7 @@ class WeSearchr(scrapy.Spider):
 
     This spider will start at both the landing page and the discovery page and pull bounties from various categories.
 
-    Currently built off of the base Spider class, but can also be built with a more tooled, generic one such as the CrawlerSpider: https://docs.scrapy.org/en/latest/topics/spiders.html#crawlspider
+    Currently built off of the base Spider class, but can also be built with a generic one such as the CrawlSpider: https://docs.scrapy.org/en/latest/topics/spiders.html#crawlspider
     '''
 
     name = 'wesearchr'
@@ -18,12 +18,16 @@ class WeSearchr(scrapy.Spider):
         ]
 
         for url in urls:
-            yield scrapy.Request(url-url, callback-self.collect_bounties)
+            yield scrapy.Request(url=url, callback=self.collect_bounties)
 
     def collect_bounties(self, response):
         '''
         This will grab links to bounties from given pages
         and yield child requests for each.
+
+        Added based on the congress spider at: https://github.com/Data4Democracy/assemble/blob/master/congress/congress/spiders/congress.py
+
+        Using CrawlSpider instead of Spider for this class could remove the need for a link-collecting function like this.
         '''
         raise NotImplementedError
 
