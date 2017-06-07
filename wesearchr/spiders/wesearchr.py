de1b18c2304595efdcd24551b338c1b08c0b449b
azstiers <abigail.stiers@gmail.com>
2017-06-06 20:29:43 -0400

    Revert "added some more field information"

--- wesearchr/spiders/wesearchr.py
+++ wesearchr/spiders/wesearchr.py
@@ -16,37 +16,27 @@ class WeSearchr(scrapy.Spider):
 
     def start_requests(self):
        
-        discover = json.loads(requests.get('http://www.wesearchr.com/api/discover/newest?').text)   
-        for bounty in discover['data']:
-            url = 'https://www.wesearchr.com/bounties/' + bounty['slug']  
-            yield scrapy.Request(url, self.parse_bounty)      
+        discover = json.loads(requests.get('http://www.wesearchr.com/api/discover/newest?page=1').text)   
+        while discover:
+            for bounty in discover['data']:
+                url = 'https://www.wesearchr.com/bounties/' + bounty['slug']  
+                yield scrapy.Request(url, self.parse_bounty) 
+            new_page = discover['next_page_url']     
+            discover = json.loads(requests.get(new_page).text)
+            
 
     #def collect_bounties(self, response):
         #start_requests should probably call this instead of directly calling parse_bounty
 
-    def get_contributors(page_id)
-        contributors = json.loads(requests.get('https://www.wesearchr.com/api/bounties/' + page_id +'contributions?').text)
-
-
     def parse_bounty(self, response):
 
         title = response.xpath('//div[@class="row"]/h1/text()').extract_first()
-
-        #these don't work for every link, some have multiple paragraphs in each section
         goal = response.xpath('//div[@class="single-project-content"]/p/text()')[0].extract()
         why = response.xpath('//div[@class="single-project-content"]/p/text()')[1].extract()
         requirements = response.xpath('//div[@class="single-project-content"]/p/text()')[3].extract()
-
-
-        contributors = get_contributors()
         
         #could be used to get time_rem, but might be more useful as deadline
         deadline = response.xpath('normalize-space(//div[@class="bounty-data data-group deadline"])').extract()
-
-        #needs to be extracted further
-        min_bounty = response.xpath('normalize-space(//div[@class="bounty-data data-group"])').extract()
-
-        updates = response.xpath('//div[@class="row column content"]/p/text()').extract_first()
 
         item = Bounty(
             url = 'unknown',
