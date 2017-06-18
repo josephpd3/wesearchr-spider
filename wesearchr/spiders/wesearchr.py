02f9a860fc5fce313b11cb7a3a3af851c788cd56
Joseph P DeBartola III <josephdebartola@gmail.com>
2017-06-18 08:46:37 -0500

    Update gitignore to ignore logs and add some error handling

--- wesearchr/spiders/wesearchr.py
+++ wesearchr/spiders/wesearchr.py
@@ -2,9 +2,13 @@ import requests
 import json
 import scrapy
 import re
+import logging
 
 from wesearchr.items import Bounty
 from json import JSONDecodeError
+
+
+logging.basicConfig(filename='log.log', level=logging.ERROR)
 
 
 class WeSearchr(scrapy.Spider):
@@ -16,6 +20,7 @@ class WeSearchr(scrapy.Spider):
     '''
 
     name = 'wesearchr'
+    logger = logging.getLogger(__name__)
 
     # A dict to hold information we can glean from the
     # initial list of bounties gotten through requests
@@ -26,18 +31,14 @@ class WeSearchr(scrapy.Spider):
         Start chain of requests from starting URLs
         """
         discover = json.loads(requests.get('http://www.wesearchr.com/api/discover/editorsChoice').text)
-        while discover:
-            for bounty in discover['data']:
-                # Since we're iterating over JSON blobs of these already,
-                # we can store some goodies while we're at it.
-                self.slug_to_summary_blob[bounty['slug']] = bounty
 
-                url = 'https://www.wesearchr.com/bounties/' + bounty['slug']
-                yield scrapy.Request(url, self.parse_bounty)
+        for bounty in discover['data']:
+            # Since we're iterating over JSON blobs of these already,
+            # we can store some goodies while we're at it.
+            self.slug_to_summary_blob[bounty['slug']] = bounty
 
-            new_page = discover['next_page_url']     
-            discover = json.loads(requests.get(new_page).text)
-            
+            url = 'https://www.wesearchr.com/bounties/' + bounty['slug']  
+            yield scrapy.Request(url, self.parse_bounty)
 
     #def collect_bounties(self, response):
         #start_requests should probably call this instead of directly calling parse_bounty
@@ -46,42 +47,48 @@ class WeSearchr(scrapy.Spider):
         """
         Parse a bounty page
         """
-        slug = response.url.split('/')[-1]
-        bounty_id = self.get_bounty_id(slug)
-        deadline = self.get_bounty_deadline(slug)
-        status = self.get_bounty_status(slug)
+        try:
+            slug = response.url.split('/')[-1]
+            bounty_id = self.get_bounty_id(slug)
+            deadline = self.get_bounty_deadline(slug)
+            status = self.get_bounty_status(slug)
 
-        title = response.xpath('//div[@class="row"]/h1/text()').extract_first()
-        goal = response.xpath('//div[@class="single-project-content"]/p/text()')[0].extract()
-        why = response.xpath('//div[@class="single-project-content"]/p/text()')[1].extract()
-        requirements = response.xpath('//div[@class="single-project-content"]/p/text()')[3].extract()
+            title = response.xpath('//div[@class="row"]/h1/text()').extract_first()
+            goal = response.xpath('//div[@class="single-project-content"]/p/text()')[0].extract()
+            why = response.xpath('//div[@class="single-project-content"]/p/text()')[1].extract()
+            requirements = response.xpath('//div[@class="single-project-content"]/p/text()')[3].extract()
 
-        snapshot = response.xpath('//div[@class="project-snapshot"]')
+            # Get min and current bounty
+            min_bounty = self.grab_min_bounty(response)
+            cur_bounty = self.grab_raised_bounty(response)
 
-        # Get min and current bounty
-        min_bounty = self.grab_min_bounty(response)
-        cur_bounty = self.grab_raised_bounty(response)
+            contributions = self.get_contributions(bounty_id)
+            updates = self.get_updates(response)
 
-        contributions = self.get_contributions(bounty_id)
-        updates = self.get_updates(response)
+            item = Bounty(
+                bounty_id=bounty_id,
+                url=response.url,
+                title=title,
+                status=status,
+                min_bounty=min_bounty,
+                cur_bounty=cur_bounty,
+                deadline=deadline,
+                contributions=contributions,
+                goal=goal,
+                why=why,
+                requirements=requirements,
+                updates=updates,
+                content_links='unknown'
+            )
 
-        item = Bounty(
-            bounty_id=bounty_id,
-            url=response.url,
-            title=title,
-            status=status,
-            min_bounty=min_bounty,
-            cur_bounty=cur_bounty,
-            deadline=deadline,
-            contributions=contributions,
-            goal=goal,
-            why=why,
-            requirements=requirements,
-            updates=updates,
-            content_links='unknown'
-        )
+            yield item
 
-        yield item
+        except Exception as e:
+            # While we keep developing, this is a decent lazy
+            # solution to figure out any errors that propagate
+            # all the way up to this function
+            self.logger.error(slug)
+            self.logger.exception('message')
 
     def get_bounty_id(self, slug):
         """
@@ -111,7 +118,11 @@ class WeSearchr(scrapy.Spider):
         page_no = 1 # start off at square one...
 
         # Get the first page
-        contrib_page = json.loads(requests.get(contrib_page_str.format(bounty_id, page_no)).text)
+        try:
+            contrib_page = json.loads(requests.get(contrib_page_str.format(bounty_id, page_no)).text)
+        except JSONDecodeError:
+            # If no contributions result, return the empty list
+            return contributions
 
         # Keep grabbing pages til it runs dry!
         while contrib_page['next_page_url'] is not None:
@@ -136,8 +147,9 @@ class WeSearchr(scrapy.Spider):
         try:
             updates_container = response.css('div.project-updates-top')[0]
         except IndexError:
-            # If nothing results, no updates for this bounty
-            return None
+            # If nothing results, no updates for this bounty;
+            # return empty list
+            return updates
 
         # Now, the information for a single update lives
         # in two adjacent divs, so we're gonna do some voodoo here
